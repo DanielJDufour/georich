50f6965b90edfa039b033e859a9e212096d2f43c
DanielJDufour <daniel.j.dufour@gmail.com>
2018-04-28 17:08:26 -0400

    updated version and added w2n to reqs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['georich'],
   package_dir = {'georich': 'georich'},
   package_data = {'georich': ['api/*', 'api/scripts/*', 'core/*']},
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Enrich your Geospatial Data!',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
@@ -13,5 +13,5 @@ setup(
   download_url = 'https://github.com/DanielJDufour/georich/tarball/download',
   keywords = ['location','geo','python','enrich', 'enrichment', 'csv', 'tsv', 'shapefile'],
   classifiers = [],
-  install_requires=["Django", "django-extensions", "freq", "numpy", "pandas", "scipy"]
+  install_requires=["Django", "django-extensions", "freq", "numpy", "pandas", "scipy", "word2number"]
 )
